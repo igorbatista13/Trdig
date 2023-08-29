5f0e14eaca87925309189fee2e8b578db6e55f6f
igorbatista13 <igorbatista13@gmail.com>
2023-08-29 17:41:11 -0400

    okokoko

--- resources/views/suporte/index.blade.php
+++ resources/views/suporte/index.blade.php
@@ -1,6 +1,13 @@
 @extends('base.novabase')
 @section('content')
-
+<?php 
+$processoCount = session()->get('processoCount'); 
+$processoCount_corrigir = session()->get('processoCount_corrigir'); 
+$processoCount_finalizado = session()->get('processoCount_finalizado'); 
+$processoCount_aguardando = session()->get('processoCount_aguardando'); 
+$processoCount_tramitada = session()->get('processoCount_tramitada'); 
+$processoCount_nao_finalizada = session()->get('processoCount_nao_finalizada'); 
+?>
   <main id="main" class="main">
 
     <div class="pagetitle">
